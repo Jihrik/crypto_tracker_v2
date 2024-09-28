9f2de9aded800d9d7f90b2baef7c0b7adf70061a
Jiri Kucera <ol.jirikucera@gmail.com>
2024-09-28 13:32:28 +0200

    changes

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,13 +1,3 @@
-"""
-WSGI config for api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-from __future__ import annotations
-
 import os
 
 from django.core.wsgi import get_wsgi_application
